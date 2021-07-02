f3c840b746503d584c5be11fb8923ac9864390f6
Jonathan García <jona03g97@gmail.com>
2021-07-01 22:16:01 -0600

    Iniciando estructura

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "react-router";
+import App from "../components/App";
+
+render(<App />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "assets/[hash].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "assets/[name].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+  ],
+  devServer: {
+    port: 3002,
+    historyApiFallback: true,
+    compress: true,
+  },
+};
